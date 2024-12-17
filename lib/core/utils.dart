6b8edb19e15b29a1e236b957b2fde3b14b576695
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-12-18 03:57:33 +0800

    feat: added formatter for currency

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -7,6 +7,14 @@ String formatDateToReadable(String date) {
   );
 }
 
+String formatCurrency(num amount) {
+  return NumberFormat.currency(
+    locale: 'en_US',
+    symbol: '\₱',
+    decimalDigits: 2,
+  ).format(amount);
+}
+
 NetTotal getNetTotal(List<TransactionModel> transactions) {
   num expense = 0, income = 0;
 
@@ -19,4 +27,21 @@ NetTotal getNetTotal(List<TransactionModel> transactions) {
   }
 
   return NetTotal(balance: income - expense, expense: expense, income: income);
+}
+
+Map<K, List<T>> groupItemsByDate<T, K>(
+    List<T> items, K Function(T) keySelector) {
+  Map<K, List<T>> groupedItems = {};
+
+  for (var item in items) {
+    K key = keySelector(item);
+
+    if (groupedItems.containsKey(key)) {
+      groupedItems[key]!.add(item);
+    } else {
+      groupedItems[key] = [item];
+    }
+  }
+
+  return groupedItems;
 }
