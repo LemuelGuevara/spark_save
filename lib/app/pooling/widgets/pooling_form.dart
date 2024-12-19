1840e686ffb90af9143c6a02fc2ac48971d5752f
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-12-19 19:44:53 +0800

    refactor(macro_form): updated padding and converted to bottomSheet (bottomBar)

--- lib/app/pooling/widgets/pooling_form.dart
+++ lib/app/pooling/widgets/pooling_form.dart
@@ -51,200 +51,208 @@ class _PoolingFormState extends State<PoolingForm> {
         automaticallyImplyLeading: true,
         scrolledUnderElevation: 0.0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            spacing: 20,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                widget.pooling == null ? 'Add Pooling' : 'Edit Pooling',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Form(
-                key: _poolingFormKey,
-                child: Column(
-                  spacing: 15,
-                  children: <Widget>[
-                    FormInput(
-                      label: 'Payer',
-                      hintText: 'e.g. John Doe',
-                      controller: _payerFieldController,
-                    ),
-                    FormInput(
-                      label: 'Expense Name',
-                      hintText: 'e.g. Bowling 12/24',
-                      controller: _expenseNameFieldController,
-                    ),
-                    FormInput(
-                      label: 'Category',
-                      hintText: 'Category',
-                      controller: _expenseCategoryFieldController,
-                    ),
-                    FormInput(
-                      label: 'Expense Amount',
-                      hintText: '0.00',
-                      controller: _expenseAmountFieldController,
-                      keyboardType: TextInputType.number,
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Members",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                    ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.73,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 20,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  widget.pooling == null ? 'Add Pooling' : 'Edit Pooling',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                   ),
-                  if (_poolingMembers.isNotEmpty)
-                    Column(
-                      children: _poolingMembers.asMap().entries.map((entry) {
-                        final int index = entry.key;
-                        final PoolingMember member = entry.value;
-
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 6),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: FormInput(
-                                  hintText: member.name,
-                                  controller:
-                                      TextEditingController(text: member.name),
-                                ),
-                              ),
-                              SizedBox(width: 10),
-                              IconButton(
-                                icon: Icon(Icons.delete, color: Colors.red),
-                                onPressed: () {
-                                  setState(() {
-                                    _poolingMembers.removeAt(index);
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                    )
-                  else
+                ),
+                Form(
+                  key: _poolingFormKey,
+                  child: Column(
+                    spacing: 15,
+                    children: <Widget>[
+                      FormInput(
+                        label: 'Payer',
+                        hintText: 'e.g. John Doe',
+                        controller: _payerFieldController,
+                      ),
+                      FormInput(
+                        label: 'Expense Name',
+                        hintText: 'e.g. Bowling 12/24',
+                        controller: _expenseNameFieldController,
+                      ),
+                      FormInput(
+                        label: 'Category',
+                        hintText: 'Category',
+                        controller: _expenseCategoryFieldController,
+                      ),
+                      FormInput(
+                        label: 'Expense Amount',
+                        hintText: '0.00',
+                        controller: _expenseAmountFieldController,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
                     Text(
-                      "No members added yet.",
-                      style: TextStyle(color: Colors.grey),
+                      "Members",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  SizedBox(height: 10),
-                  RoundedButton(
-                    onTap: () {
-                      showCupertinoModalBottomSheet(
-                        isDismissible: false,
-                        context: context,
-                        builder: (context) => Padding(
-                          padding: EdgeInsets.only(
-                            bottom: MediaQuery.of(context).viewInsets.bottom,
-                          ),
-                          child: SafeArea(
-                            child: SizedBox(
-                              height: MediaQuery.of(context).size.height * 0.6,
-                              child: Scaffold(
-                                appBar: AppBar(
-                                  automaticallyImplyLeading: false,
-                                  scrolledUnderElevation: 0.0,
-                                  title: Text("Add Member"),
-                                  centerTitle: false,
-                                  actions: <Widget>[
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: IconButton(
-                                        onPressed: () =>
-                                            Navigator.of(context).pop(),
-                                        icon: const Icon(Icons.close_rounded),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                body: Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(20.0),
-                                    child: Form(
-                                      key: _memberFormKey,
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.stretch,
-                                        children: [
-                                          FormInput(
-                                            label: 'Name of Member',
-                                            hintText: 'e.g. John Doe',
-                                            controller: _memberFieldController,
-                                            validator: (value) {
-                                              if (value == null ||
-                                                  value.trim().isEmpty) {
-                                                return 'Member name cannot be empty';
-                                              }
-                                              return null;
-                                            },
-                                          ),
-                                        ],
-                                      ),
-                                    ),
+                    if (_poolingMembers.isNotEmpty)
+                      Column(
+                        children: _poolingMembers.asMap().entries.map((entry) {
+                          final int index = entry.key;
+                          final PoolingMember member = entry.value;
+        
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 6),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: FormInput(
+                                    hintText: member.name,
+                                    controller:
+                                        TextEditingController(text: member.name),
                                   ),
                                 ),
-                                bottomNavigationBar: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 20.0,
-                                    vertical: 10.0,
+                                SizedBox(width: 10),
+                                IconButton(
+                                  icon: Icon(Icons.delete, color: Colors.red),
+                                  onPressed: () {
+                                    setState(() {
+                                      _poolingMembers.removeAt(index);
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      )
+                    else
+                      Text(
+                        "No members added yet.",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    SizedBox(height: 10),
+                    RoundedButton(
+                      onTap: () {
+                        showCupertinoModalBottomSheet(
+                          isDismissible: false,
+                          context: context,
+                          builder: (context) => Padding(
+                            padding: EdgeInsets.only(
+                              bottom: MediaQuery.of(context).viewInsets.bottom,
+                            ),
+                            child: SafeArea(
+                              child: SizedBox(
+                                height: MediaQuery.of(context).size.height * 0.6,
+                                child: Scaffold(
+                                  appBar: AppBar(
+                                    automaticallyImplyLeading: false,
+                                    scrolledUnderElevation: 0.0,
+                                    title: Text("Add Member"),
+                                    centerTitle: false,
+                                    actions: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: IconButton(
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(),
+                                          icon: const Icon(Icons.close_rounded),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  child: RoundedButton(
-                                    onTap: () {
-                                      if (_memberFormKey.currentState!
-                                          .validate()) {
-                                        setState(() {
-                                          _poolingMembers.add(
-                                            PoolingMember(
-                                              id: UniqueKey().toString(),
-                                              name: _memberFieldController.text
-                                                  .trim(),
-                                              shareAmount: 0,
-                                              isPaid: false,
+                                  body: Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(20.0),
+                                      child: Form(
+                                        key: _memberFormKey,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.stretch,
+                                          children: [
+                                            FormInput(
+                                              label: 'Name of Member',
+                                              hintText: 'e.g. John Doe',
+                                              controller: _memberFieldController,
+                                              validator: (value) {
+                                                if (value == null ||
+                                                    value.trim().isEmpty) {
+                                                  return 'Member name cannot be empty';
+                                                }
+                                                return null;
+                                              },
                                             ),
-                                          );
-                                          _memberFieldController.clear();
-                                        });
-                                        Navigator.pop(context);
-                                      }
-                                    },
-                                    label: 'Add Member',
-                                    backgroundColor: Colors.green.shade800,
-                                    textColor: Colors.white,
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  bottomNavigationBar: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 20.0,
+                                      vertical: 10.0,
+                                    ),
+                                    child: RoundedButton(
+                                      onTap: () {
+                                        if (_memberFormKey.currentState!
+                                            .validate()) {
+                                          setState(() {
+                                            _poolingMembers.add(
+                                              PoolingMember(
+                                                id: UniqueKey().toString(),
+                                                name: _memberFieldController.text
+                                                    .trim(),
+                                                shareAmount: 0,
+                                                isPaid: false,
+                                              ),
+                                            );
+                                            _memberFieldController.clear();
+                                          });
+                                          Navigator.pop(context);
+                                        }
+                                      },
+                                      label: 'Add Member',
+                                      backgroundColor: Colors.green.shade800,
+                                      textColor: Colors.white,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                    label: "Add Member",
-                    backgroundColor: Colors.grey.shade100,
-                    textColor: Colors.black,
-                  ),
-                ],
-              ),
-            ],
+                        );
+                      },
+                      label: "Add Member",
+                      backgroundColor: Colors.grey.shade100,
+                      textColor: Colors.black,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
-      bottomNavigationBar: SafeArea(
+      bottomSheet: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(20.0),
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: 40,
+            top: 10,
+          ),
           child: RoundedButton(
             onTap: () {
               // Check if there are at least 2 members
